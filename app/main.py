511c646972de60178e777c802fe8f66bc9f1fa5e
Arif A <aarifaslam2002@gmail.com>
2026-03-12 11:58:10 +0530

    update url

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ create_tables()
 app.add_middleware(CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://distributeease.vercel.app",  # ← your vercel URL
+        "https://distri-ease.vercel.app/",  # ← your vercel URL
     ],
     allow_methods=["*"],
     allow_headers=["*"],
